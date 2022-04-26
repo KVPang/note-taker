7b7acae898eb47c117781705cde4b6cc6d9dc6b5
Kristina Pang <kvypang@gmail.com>
2022-04-26 15:40:54 -0700

    variables

---
+++ server.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+const express = require('express');
+const app = express();
+
+const PORT = process.env.port || 3001;
+const myNotes = require('./db/db.json');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
