a99e7dc9d69e09974d6d589e06fde53b51f0cdd9
Kristina Pang <kvypang@gmail.com>
2022-04-26 16:55:00 -0700

    updates

--- server.js
+++ server.js
@@ -1,24 +1,34 @@
-const fs = require('fs');
-const path = require('path');
-
 const express = require('express');
+const path = require('path');
+const api = require('./routes/index.js');
+
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 
-const PORT = process.env.port || 3001;
-const myNotes = require('./db/db.json');
-
+// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// GET Route for feedback page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
 
 
-function makeNote () {
-    const newNote = 
+// bonus - deleting notes 
+// function deleteNote() {
 
-}
-
-
-// bonus 
-function deleteNote() {
-
-
-}
+// }
